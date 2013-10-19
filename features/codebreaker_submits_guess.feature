610af05b331096187f41740c9eefdc899c25cfcd
Antonis Berkakis <aberkakis@gmail.com>
2013-10-19 10:08:49 +0100

    end of chapter 7

--- features/codebreaker_submits_guess.feature
+++ features/codebreaker_submits_guess.feature
@@ -14,7 +14,7 @@ Feature: code-breaker submits guess
 
 		Scenarios: 1 number correct
 			| code | guess | mark |
-			| 1234 |  5555 | +		|
+			| 1234 |  1555 | +		|
 			| 1234 |  2555 | -		|
 
 		Scenarios: 2 numbers correct
--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -11,11 +11,27 @@ module Codebreaker
 		end
 
 		def guess(guess)
-			if @secret.include?(guess[0])
-				@output.puts '-'
-			else
-				@output.puts ''
+			mark = ''
+			(0..3).each do |index|
+				if exact_match?(guess, index)
+					mark << '+'
+				end
 			end
+
+			(0..3).each do |index|
+				if number_match?(guess, index)
+					mark << '-'
+				end
+			end
+			@output.puts mark
+		end
+
+		def exact_match?(guess, index)
+			guess[index] == @secret[index]
+		end
+
+		def number_match?(guess, index)
+			@secret.include?(guess[index]) && !exact_match?(guess, index)
 		end
 	end
 end
